19c7e5eb1bd7a129edede3d0edfd82ecedfba1fc
Krimothiazine <krimouv1995@gmail.com>
2023-08-31 11:46:08 +0100

    Fist layout of About section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,30 +2,28 @@ import styles from './About.module.scss';
 
 const About = () => {
   return (
-    <section id="about">
-      <h2>About Me</h2>
-      <p>
-        Welcome to my portfolio! I'm not your usual pharmacist; I'm also a
-        programming enthusiast. I'm on a mission to tackle problems using both
-        my pharmacy know-how and coding skills. Learning is my constant
-        companion, as I strive to level up and enhance my abilities. Join me as
-        I merge my pharmacy background with the world of programming to craft
-        innovative solutions that truly make an impact!
-      </p>
-      <div className={styles.skills}>
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" />
-
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
-
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
-
-        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
+    <section id="about" className={styles.about}>
+      <div className={styles.content}>
+        <h2>About Me</h2>
+        <div>
+          <p>
+            Welcome to my portfolio! I'm not your usual pharmacist; I'm also a
+            programming enthusiast. I'm on a mission to tackle problems using
+            both my pharmacy know-how and coding skills. Learning is my constant
+            companion, as I strive to level up and enhance my abilities. Join me
+            as I merge my pharmacy background with the world of programming to
+            craft innovative solutions that truly make an impact!
+          </p>
+          <div className={styles.skills}>
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sass/sass-original.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
+            <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" />
+          </div>
+        </div>
       </div>
     </section>
   );
