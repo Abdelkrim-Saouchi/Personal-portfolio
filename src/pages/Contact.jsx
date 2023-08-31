4d08821c473933cb202ebea92c44567720ebd4b4
Krimothiazine <krimouv1995@gmail.com>
2023-08-31 21:16:39 +0100

    First layout of contact section

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,22 +1,58 @@
 import { mdiFacebook, mdiGithub, mdiGmail, mdiLinkedin } from '@mdi/js';
 import Icon from '@mdi/react';
+import { useRef } from 'react';
+import { Email } from '../sendEmail/smtp';
+import styles from './Contact.module.scss';
 
 const Contact = () => {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const messageRef = useRef(null);
+
+  const sendEmail = (e) => {
+    console.log(emailRef.current.value);
+    console.log(messageRef.current.value);
+    e.preventDefault();
+    console.log('run');
+    Email.send({
+      Host: 'smtp.elasticemail.com',
+      Username: 'karimsa1955@outlook.com',
+      Password: 'A9FB2E2E8614FC6E967612165C962003F497',
+      To: 'krimouv1995@gmail.com',
+      From: 'karimsa1955@outlook.com',
+      Subject: 'Message from my Portfolio',
+      Body: `
+        name: ${nameRef.current.value}.
+        email: ${emailRef.current.value}.
+        ${messageRef.current.value}
+      `,
+    }).then((message) => alert(message));
+  };
   return (
-    <section id="contact">
-      <h2>Contact Me</h2>
-      <p>Do not hesitate if you want to contact me and discuss.</p>
-      <form action="">
-        <input type="text" placeholder="Name" />
-        <input type="email" placeholder="Email" />
-        <textarea placeholder="Message" id="" cols="30" rows="10"></textarea>
-        <button type="submit">Submit</button>
-      </form>
-      <div>
-        <Icon path={mdiGmail} size={1} />
-        <Icon path={mdiGithub} size={1} />
-        <Icon path={mdiFacebook} size={1} />
-        <Icon path={mdiLinkedin} size={1} />
+    <section id="contact" className={styles.contact}>
+      <div className={styles.content}>
+        <h2>Contact Me</h2>
+        <p>Do not hesitate if you want to contact me and discuss.</p>
+        <form onSubmit={sendEmail}>
+          <input type="text" placeholder="Name" ref={nameRef} />
+          <input type="email" placeholder="Email" ref={emailRef} />
+          <textarea placeholder="Message" ref={messageRef}></textarea>
+          <button type="submit">Submit</button>
+        </form>
+        <div>
+          <a href="#">
+            <Icon path={mdiGmail} size={1.5} />
+          </a>
+          <a href="#">
+            <Icon path={mdiGithub} size={1.5} />
+          </a>
+          <a href="#">
+            <Icon path={mdiFacebook} size={1.5} />
+          </a>
+          <a href="#">
+            <Icon path={mdiLinkedin} size={1.5} />
+          </a>
+        </div>
       </div>
     </section>
   );
