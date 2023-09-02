bcf2ce34ab89a8cb7f313c490f93b2b6ea032509
Krimothiazine <krimouv1995@gmail.com>
2023-09-02 19:46:49 +0100

    Make hero section animation

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -4,8 +4,8 @@ const Hero = () => {
   return (
     <section className={styles.hero}>
       <div>
-        <h1>Hi, I'm Krimothiazine</h1>
-        <h2>I'm a Web Developer</h2>
+        <h1></h1>
+        <h2></h2>
       </div>
     </section>
   );
